34f7b16adffaa4f473eb9148b955f28288b05db0
c-j-j <chrisjordan1987@gmail.com>
2015-02-27 15:48:07 +0000

    grunt file to compile coffee script then run tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,30 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       js: {
         files: ['spec/**/*.js', 'public/**/*.js'],
         tasks: ['karma:continuous'],
+      },
+      coffee: {
+        files: ['public/coffee/*.coffee', 'spec/coffee/*.coffee'],
+        tasks: ['coffee'],
       }
     },
+
+    coffee: {
+      compile: {
+        options: {
+          join: true
+        },
+        files: {
+          'public/javascript/main.js': ['public/coffee/*.coffee'],
+          'spec/javascript/coffee_scripts_spec.js': ['spec/coffee/*.coffee']
+        }
+      }
+    },
+
 
     karma: {
       continuous: {
@@ -19,5 +37,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
   grunt.registerTask('default', ['watch']);
 };
---
+++ public/javascript/main.js
@@ -0,0 +1,10 @@
+(function() {
+  var a, b, c;
+
+  a = 1;
+
+  b = 2;
+
+  c = 3;
+
+}).call(this);
